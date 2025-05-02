e146ef71c3589d2a35a0e8193bebce1ba71ad717
poojasarvadhi <pooja.sarvadhi@gmail.com>
2025-05-02 10:23:03 +0530

    Doctype created

---
+++ doc_review/doc_review/doctype/review_document/test_review_document.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, sarvadhi@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestReviewDocument(FrappeTestCase):
+	pass
